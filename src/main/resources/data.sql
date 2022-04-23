a9580c1de8069211ebf3a1a68c6c8728bb195d2d
Erick Rangel <erick.rangel@globant.com>
2022-04-22 19:20:00 -0500

    Added properties h2 console Created data.sql Created entity person Created PersonJdbcDao

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+create table person 
+(
+	id integer not null,
+	name varchar(255) not null,
+	location varchar(255),
+	birth_date timestamp,
+	primary key(id)
+);
+
+INSERT INTO PERSON (ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(10001,'Ranga','Hyderab',sysdate());
+INSERT INTO PERSON (ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(10002,'Mateo','Colombia',sysdate());
+INSERT INTO PERSON (ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(10003,'Valeria','Colombia',sysdate());
+INSERT INTO PERSON (ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(10004,'Camila','Colombia',sysdate());
